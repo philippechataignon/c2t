18bb2f7f3778ce65b1cf2d29e24c51333af2234d
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-02-11 15:26:52 +0100

    Use lz4hc for high compression

--- lz4_compress.c
+++ lz4_compress.c
@@ -1,9 +1,8 @@
-#define LZ4_MEMORY_USAGE 20
-#include <stdio.h>   // For printf()
-#include <string.h>  // For memcmp()
-#include <stdlib.h>  // For exit()
-#include <errno.h>     // This is all that is required to expose the prototypes for basic compression and decompression.
-#include <lz4.h>     // This is all that is required to expose the prototypes for basic compression and decompression.
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <lz4hc.h>
 
 char data[65536];
 
@@ -25,17 +24,14 @@ int main(int argc, const char **argv) {
         fprintf(stderr, "Error %d (%s): %s\n", errno, inpFilename, strerror(errno));
         return errno;
     }
-
-    FILE* const outFp = fopen(lz4Filename, "wb");
-
     const int src_size = fread(data, 1, 65536, inpFp);
-    const int max_dst_size = LZ4_compressBound(src_size);
-    // We will use that size for our destination boundary when allocating space.
-    char* compressed_data = (char*)malloc((size_t)max_dst_size);
-    const int compressed_data_size = LZ4_compress_default(data, compressed_data, src_size, max_dst_size);
-    const int out_size = fwrite(compressed_data, 1, compressed_data_size, outFp);
-    printf("compress : %s -> %s %d->%d\n", inpFilename, lz4Filename, src_size, out_size);
-    fclose(outFp);
     fclose(inpFp);
+    const int max_dst_size = LZ4_compressBound(src_size);
+    char* compressed_data = (char*)malloc((size_t)max_dst_size);
+    const int compressed_data_size = LZ4_compress_HC(data, compressed_data, src_size, max_dst_size, LZ4HC_CLEVEL_MAX);
+    FILE* const outFp = fopen(lz4Filename, "wb");
+    const int out_size = fwrite(compressed_data, 1, compressed_data_size, outFp);
+    fprintf(stderr, "Compress: %s %d->%d\n", lz4Filename, src_size, out_size);
+    fclose(outFp);
     return 0;
 }
