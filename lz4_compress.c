d7ddff78fff356e5b98df576db346b198d904fcb
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-02-20 14:12:25 +0100

    Display LZ4_compressBound result

--- lz4_compress.c
+++ lz4_compress.c
@@ -31,7 +31,7 @@ int main(int argc, const char **argv) {
     const int compressed_data_size = LZ4_compress_HC(data, compressed_data, src_size, max_dst_size, LZ4HC_CLEVEL_MAX);
     FILE* const outFp = fopen(lz4Filename, "wb");
     const int out_size = fwrite(compressed_data, 1, compressed_data_size, outFp);
-    fprintf(stderr, "Compress: %s %d->%d\n", lz4Filename, src_size, out_size);
+    fprintf(stderr, "Compress (max: $%x): %s %d->%d\n", max_dst_size, lz4Filename, src_size, out_size);
     fclose(outFp);
     return 0;
 }
