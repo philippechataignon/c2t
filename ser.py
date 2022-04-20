6841a07da5d5c421aef85010f81914c03f2abf28
Philippe Chataignon <philippe@inspiron>
2022-04-20 11:46:30 +0200

    Print progression

--- ser.py
+++ ser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import serial
 def main():
     ack = b'\x06'
@@ -11,26 +12,28 @@ def main():
     c1 = ser.read(1)
     c2 = ser.read(1)
     c3 = ser.read(1)
-    print(c1, c2, c3)
     if c1 == b'\x19' and c2 == b'\x64' and c3 == b'\x00':
-        print("Start OK")
+        print("Start OK", file=sys.stderr)
         ser.write(ack)
 
     with open(f"ser.out", "wb") as g:
         for i in range(5):
             print(f"Wait segment #{i+1}")
-            c = ser.read(0x7000)
-            g.write(c)
-            print(f"Get segment #{i+1}")
+            for j in range(7):
+                c = ser.read(0x1000)
+                g.write(c)
+                print(".", end="", file=sys.stderr, flush=True)
+            print(file=sys.stderr)
+            print(f"Get segment #{i+1}", file=sys.stderr)
             ser.write(ack)
 
     # final ack
     c1 = ser.read(1)
     if c1 == ack:
-        print("OK, done.")
+        print("OK, done.", file=sys.stderr)
         ser.write(ack)
     else:
-        print("Error: no ack")
+        print("Error: no ack", file=sys.stderr)
 
 
     ser.close()
