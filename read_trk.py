01249dcb2fd9ad805fafd6cf1e8574ae24da4862
Philippe Chataignon <philippe@ldlc>
2022-08-10 14:28:33 +0200

    Fix pos in read_track

--- read_trk.py
+++ read_trk.py
@@ -3,15 +3,16 @@ import sys
 def main():
     f = open(sys.argv[1], "rb")
     g = open(f"{sys.argv[1]}.nib", "wb")
+    print(sys.argv[1], end=" ")
     c = f.read()
     # print(len(c))
-    n = 12
-    if c[:n] in c[n+1:]:
+    n = 14
+    pos = c[n:].find(c[:n])
+    if 6000 <= pos:
         print("OK")
-        i = n + c[n+1:].index(c[:n]) + 1
-        g.write(c[:i])
+        g.write(c[:n+pos])
     else:
-        print("6400")
+        print(f"6400 ({pos})")
         g.write(c[:6400])
 
 if __name__ == '__main__':
